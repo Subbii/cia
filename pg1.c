47160b8c29603bad9d7e56ca4b28cb7b7b62d92f
subbaiahct <krthksubbiah@gmail.com>
2018-09-18 13:11:35 +0530

    mod

---
+++ pg1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum=0;
+	printf("Enter 2 numbers to add\n");
+	scanf("%d %d",&a,&b);
+	sum=a-b;
+	printf("Sum=%d",sum);
+}
+
---
+++ pg2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,diff;
+	printf("Enter 2 numbers\n");
+	scanf("%d %d",&a,&b);
+	diff=a+b;
+	printf("Diff=%d\t",diff);
+}
+
