5159c58152935d25b93c490507bc45ad0900bcaa
subbaiahct <krthksubbiah@gmail.com>
2018-09-18 14:16:55 +0530

    all programs

---
+++ pg3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,mul;
+	printf("Enter 2 number\n");
+	scanf("%d %d",&a,&b);
+	mul=a*b;
+	printf("Mul=%d",mul);
+}
---
+++ pg4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,div;
+	printf("Ente 2 numbers\n");
+	scanf("%d %d",&a,&b);
+	div=a/b;
+	printf("Div=%d",div);
+}
